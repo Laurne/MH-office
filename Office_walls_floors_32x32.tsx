04624c02db1ea21563d485eb85411b4d1280686d
Laurne <81371851+Laurne@users.noreply.github.com>
2021-04-14 10:49:59 +0200

    Create Office_walls_floors_32x32.tsx

---
+++ Office_walls_floors_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Office" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="Office_walls_floors_32x32.png" width="512" height="384"/>
+</tileset>
